d4ecd284cab37025a3f2d6d92d2176109835e308
mkpaulkim <mkkim.hcube@gmail.com>
2021-10-20 15:41:34 -0400

    2021-10-20 4 pm

--- ASigma_Proc.py
+++ ASigma_Proc.py
@@ -3,36 +3,36 @@ from tkinter import filedialog
 import pycubelib.files_functions as ff
 import pycubelib.general_functions as gf
 import pycubelib.plotting_functions as pf
+import pycubelib.data_functions as df
 
 pi = np.pi
 pi2 = pi * 2
 pi2limit = (-pi, pi)
+sxy = (.35, .35)
 
 txt_path = filedialog.askopenfilename(title='TXT file path', filetypes=[('txt files', '*.txt')])
-print(f'> txt_path = {txt_path}')
 notes, txt_path = ff.read_txt(txt_path)
-print(f'> notes: \n{notes}')
 nx = gf.find_param(notes, 'nx', int)
 ny = gf.find_param(notes, 'ny', int)
 dx = gf.find_param(notes, 'dx_um', float)
 nh = gf.find_param(notes, 'nh', int)
 lam_ns = gf.find_param(notes, 'lam_ns', float)
+print(f'> txt_path = {txt_path}')
+print(f'> notes: \n{notes}')
+blank = np.zeros((ny, nx))
 
-sxy = (.3, .3)
-hhhp = [np.zeros((ny, nx))]
+hhhp = []
 for n in range(nh+1):
     if n == 0:
         hh_path = txt_path.replace('.txt', f'_aa.png')
+        hh, _ = ff.read_png(hh_path, alimit=(0., 1.))
     else:
         hh_path = txt_path.replace('.txt', f'_{n}p.png')
-    hhp, _ = ff.read_png(hh_path, alimit=pi2limit)
+        hh, _ = ff.read_png(hh_path, alimit=pi2limit)
     capA, _ = gf.path_parts(hh_path)
-    pf.plotAAB(hhp, f'hh{n}p', capA=capA, sxy=sxy, pause=.5)
-    hhhp += [hhp]
+    pf.plotAAB(hh, 'HHn', capA=capA, sxy=sxy, pause=.5)
+    hhhp += [hh]
 hha = hhhp[0]
-
-zz12 = np.mod(hhhp[1] - hhhp[2] + pi, pi2) - pi
-pf.plotAAB(zz12, 'zz12', capA='ZZ12', sxy=sxy)
 
 lam_1ns = np.zeros(nh + 1)
 lam1 = lam_ns[1]
@@ -40,84 +40,23 @@ for n in range(2, nh+1):
     lam_1ns[n] = (lam1 * lam_ns[n]) / (lam1 - lam_ns[n])
 print(gf.prn_list('lam_1ns', lam_1ns, 1))
 
+zz_1ns = [blank, blank]
+for n in range(2, nh+1):
+    zz1n = (np.mod(hhhp[1] - hhhp[n] + pi, pi2) - pi) * lam_1ns[n] / pi2
+    zz_1ns += [zz1n]
+    pf.plotAAB(zz1n, 'ZZ1n', capA=f'ZZ1{n}', sxy=sxy, pause=.5)
+
+zz_12ns = [blank, blank, zz_1ns[2]]
+for n in range(3, nh+1):
+    zz_12n = df.stitch(zz_12ns[n-1], zz_1ns[n], lam_1ns[2], lam_1ns[n])
+    zz_12ns += [zz_12n]
+    pf.plotAAB(zz_12n, 'ZZ12n', capA=f'ZZ12{n}', sxy=sxy, pause=.5)
+
 pf.plt.show()
 
 
 
-'''
-def program_loop():
-    global g_t
 
-    if fp.btn_setcam.is_on():
-        m.set_cam()
-        fp.btn_setcam.off()
-
-    if fp.btn_camera.is_on():
-        g_t, g_cc = m.show_cam(g_t)
-
-    if fp.btn_angles.is_on():
-        m.calc_angles()
-        fp.btn_angles.off()
-
-    if fp.btn_vset.is_on():
-        v_piezo = fp.ent_vpiezo.get_val(float)
-        hw.piezo.set_vpz(v_piezo)
-        fp.btn_vset.off()
-
-    if fp.btn_hpath.is_on():
-        txt_path = filedialog.asksaveasfilename(title='HH.txt file path', filetypes=[('txt files', '*.txt')])
-        # txt_path = filedialog.askopenfilename(title='HH.txt file path', filetypes=[('txt files', '*.txt')])
-        if txt_path.find('.txt') < 0: txt_path += '.txt'
-        fp.ent_hfile.set_entry(txt_path)
-        if fp.btn_readh.is_on(): m.read_hparams()
-        fp.btn_hpath.off()
-
-    if fp.btn_bpath.is_on():
-        txt_path = filedialog.askopenfilename(title='BB.txt file path', filetypes=[('txt files', '*.txt')])
-        if txt_path.find('.txt') < 0: txt_path += '.txt'
-        fp.ent_bfile.set_entry(txt_path)
-        fp.btn_bpath.off()
-
-    if fp.btn_scanh.is_on():
-        m.start_scan()
-        # fp.btn_scanh.off()
-
-    if fp.btn_procz.is_on():
-        m.proc_zzz()
-        fp.btn_procz.off()
-
-    if fp.btn_cfg.is_on():
-        m.close_figs()
-        fp.btn_cfg.off()
-
-    if fp.btn_graphall.is_on():
-        c = f'{fp.ent_cols.get_val()}'
-        cc = []
-        for i in range(len(c)):
-            cc += [int(c[i])]
-        cc = tuple(cc)
-        nrow = fp.ent_nrow.get_val()
-        pf.graph_many('graph_all', m.g_graphs, cols=cc, nrow=nrow, resize=0.5)
-        fp.btn_graphall.off()
-
-    fp.tkwindow.after(tloop, program_loop)
-
-
-def adios():
-    hw.qcam.dispose()
-    fp.tkwindow.destroy()
-    print(f'> adios amigos ...')
-    quit()
-
-
-fp.btn_adios.command(adios)
-g_t = hw.qcam.tframe
-
-tloop = 10
-fp.tkwindow.after(tloop, program_loop)
-fp.tkwindow.mainloop()
-
-'''
 
 
 
--- pycubelib/data_functions.py
+++ pycubelib/data_functions.py
@@ -26,6 +26,15 @@ def get_roi(aa, roi):
     return aa
 
 
+def stitch(zz12n_in, zz1n, lam12, lam1n):
+    yy = np.round(zz12n_in / lam1n) * lam1n
+    zz = np.mod(yy + zz1n + lam12/2, lam12) - lam12/2
+    dzz = np.mod(zz - zz12n_in + lam12/2, lam12) - lam12/2
+    ezz = (np.abs(dzz) > (0.5 * lam1n)) * lam1n * np.sign(dzz)
+    zz12n_out = np.mod(zz - ezz + lam12/2, lam12) - lam12/2
+    return zz12n_out
+
+
 if __name__ == '__main__':
 
     pass
