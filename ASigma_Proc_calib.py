609a900f44ec2b13c23fbd11f7906d547c14a330
mkpaulkim <mkkim.hcube@gmail.com>
2021-10-25 16:51:46 -0400

    2021-10-25 5 pm

--- ASigma_Proc_calib.py
+++ ASigma_Proc_calib.py
@@ -9,12 +9,12 @@ pi = np.pi
 pi2 = pi * 2
 pi2limit = (-pi, pi)
 sxy = (.35, .35)
-roi = (1000, 900, 10, 10)
+roi = (960, 500, 10, 10)
 z0_roi = 0.
 
-# txt_path = filedialog.askopenfilename(title='TXT file path', filetypes=[('txt files', '*.txt')])
-txt_path = '/media/mkpaulkim/Ultra Touch/{{UT White}}/Dropbox/[[ PROJECTS.dbox ]]' \
-           '/project folders 2021/proj 2021-10 AlphaSigma/temp_data/eee7.txt'
+txt_path = filedialog.askopenfilename(title='TXT file path', filetypes=[('txt files', '*.txt')])
+# txt_path = '/media/mkpaulkim/Ultra Touch/{{UT White}}/Dropbox/[[ PROJECTS.dbox ]]' \
+#            '/project folders 2021/proj 2021-10 AlphaSigma/temp_data/eee7.txt'
 notes, txt_path = ff.read_txt(txt_path)
 note = notes[notes.find('%%%'):]
 nx = gf.find_param(note, 'nx', int)
@@ -62,7 +62,8 @@ ave_1ns = [0, 0]
 std_1ns = [0, 0]
 for n in range(2, nw + 1):
     lam1n = lam_1ns[n]
-    ep1n = np.mod(hhhp[n] - hhhp[1] + pi, pi2) - pi
+    # ep1n = np.mod(hhhp[n] - hhhp[1] + pi, pi2) - pi
+    ep1n = np.mod(hhhp[1] - hhhp[n] + pi, pi2) - pi
     zp1n = ep1n * lam1n / pi2
     z_roi, _ = gf.roi_cyclic_measure(zp1n, roi, lam1n)
     zz1n = np.mod(zp1n - z_roi + z0_roi + lam1n/2, lam1n) - lam1n/2
@@ -87,7 +88,7 @@ zz_12ns = [blank, blank, zz_1ns[2]]
 ave_12ns = [0, 0, ave_1ns[2]]
 std_12ns = [0, 0, std_1ns[2]]
 for n in range(3, nw + 1):
-    lam_1ns[n] = df.calib_lam1n(zz_12ns[n-1], zz_1ns[n], lam12, lam_1ns[n], roi)
+    # lam_1ns[n] = df.calib_lam1n(zz_12ns[n-1], zz_1ns[n], lam12, lam_1ns[n], roi)
     zz12n = df.stitch(zz_12ns[n-1], zz_1ns[n], lam12, lam_1ns[n])
 
     ave12n, std12n = gf.roi_measure(zz12n, roi)
@@ -106,7 +107,7 @@ print(gf.prn_list('std_12ns', std_12ns, 1))
 ''' graph all '''
 graphs = []
 ix, iy, rx, ry = roi
-# print(f'< roi = {roi}')
+print(f'< roi = {roi}')
 for n in range(1, nw + 1):
     graphs += [(hhhp[n][iy, :], (n-1, 0), f'HH{n}p', pi2limit)]
 for n in range(2, nw + 1):
